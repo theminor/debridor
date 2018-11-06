efcc1666995b9f1050478cbd4cb77488768a01d7
John <theminor@users.noreply.github.com>
2018-11-06 14:07:47 -0500

    round percentage display

--- clientScript.js
+++ clientScript.js
@@ -29,9 +29,9 @@ skt.onmessage = function(event) {
 			newBar.setAttribute('aria-valuenow', current / max * 100);
 			newBar.setAttribute('aria-valuemin', 0);
 			newBar.setAttribute('aria-valuemax', max);
-			newBar.innerText = text || ((current / max * 100).toString() + '%');
+			newBar.innerText = text || ((current / max * 100).toFixed(1).toString() + '%');
 		}
 		if (msg.unrestricting) msg.unrestricting.forEach(unr => addBar(100, 100, unr));
-		if (msg.downloading) msg.downloading.forEach(unr => addBar(unr.file.bytesWritten, unr.fileSize, (unr.file.path + ': ' + (unr.file.bytesWritten / unr.fileSize * 100) + '% (' + unr.file.bytesWritten + ' of ' + unr.fileSize + ' bytes)')));
+		if (msg.downloading) msg.downloading.forEach(unr => addBar(unr.file.bytesWritten, unr.fileSize, (unr.file.path + ': ' + (unr.file.bytesWritten / unr.fileSize * 100).toFixed(1); + '% (' + unr.file.bytesWritten + ' of ' + unr.fileSize + ' bytes)')));
 	}
 }
