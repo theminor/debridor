027ea80f8e62a7e0bd24b4d607d2474b0dd6c904
John <theminor@users.noreply.github.com>
2018-11-06 14:04:31 -0500

    Use status bars for progress

--- clientScript.js
+++ clientScript.js
@@ -14,22 +14,24 @@ const skt = new WebSocket(window.location.href.replace('http://', 'ws://').repla
 
 skt.onmessage = function(event) {
 	let msg = JSON.parse(event.data);
-	if ((typeof msg === 'string') && (msg.startsWith('{'))) msg = JSON.parse(msg);	// needed for bug re "over-stringified" json
-	let statusElement = document.getElementById('statusText');
-	statusElement.innerHTML = statusElement.innerHTML + '\n' + msg;
-
-	let progBarsDiv = document.getElementById('progBars');
-	while (progBarsDiv.hasChildNodes()) { progBarsDiv.removeChild(progBarsDiv.lastChild); }  // remove all bars and re-build each, below
-	function addBar(current, max, text) {
-		let newBar = progBarsDiv.appendChild(document.createElement('div'));
-		newBar.className = 'progress-bar';
-		newBar.setAttribute('role', 'progressbar');
-		newBar.setAttribute('width', (current / max * 100).toString() + '%');
-		newBar.setAttribute('aria-valuenow', current / max * 100);
-		newBar.setAttribute('aria-valuemin', 0);
-		newBar.setAttribute('aria-valuemax', max);
-		newBar.innerText = text || ((current / max * 100).toString() + '%');
+	if (typeof msg === 'string') {
+		if (msg.startsWith('{')) msg = JSON.parse(msg);  // needed for bug re "over-stringified" json
+		else document.getElementById('statusText').innerHTML = statusElement.innerHTML + '\n' + msg;
 	}
-	if (msg.unrestricting) msg.unrestricting.forEach(unr => addBar(100, 100, unr));
-	if (msg.downloading) msg.downloading.forEach(unr => addBar(unr.file.bytesWritten, unr.fileSize, (unr.file.path + ': ' + (unr.file.bytesWritten / unr.fileSize * 100) + '% (' + unr.file.bytesWritten + ' of ' + unr.fileSize + ' bytes)')));
-};
+	if (typeof msg === 'object') {
+		let progBarsDiv = document.getElementById('progBars');
+		while (progBarsDiv.hasChildNodes()) { progBarsDiv.removeChild(progBarsDiv.lastChild); }  // remove all bars and re-build each, below
+		function addBar(current, max, text) {
+			let newBar = progBarsDiv.appendChild(document.createElement('div'));
+			newBar.className = 'progress-bar';
+			newBar.setAttribute('role', 'progressbar');
+			newBar.setAttribute('width', (current / max * 100).toString() + '%');
+			newBar.setAttribute('aria-valuenow', current / max * 100);
+			newBar.setAttribute('aria-valuemin', 0);
+			newBar.setAttribute('aria-valuemax', max);
+			newBar.innerText = text || ((current / max * 100).toString() + '%');
+		}
+		if (msg.unrestricting) msg.unrestricting.forEach(unr => addBar(100, 100, unr));
+		if (msg.downloading) msg.downloading.forEach(unr => addBar(unr.file.bytesWritten, unr.fileSize, (unr.file.path + ': ' + (unr.file.bytesWritten / unr.fileSize * 100) + '% (' + unr.file.bytesWritten + ' of ' + unr.fileSize + ' bytes)')));
+	}
+}
