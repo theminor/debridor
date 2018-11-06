1346b7d55b1d73b945470dd0235bc9c3eb9b5968
John <theminor@users.noreply.github.com>
2018-11-06 13:40:32 -0500

    debugging

--- clientScript.js
+++ clientScript.js
@@ -32,5 +32,9 @@ skt.onmessage = function(event) {
 		newBar.innerText = text || ((current / max * 100).toString() + '%');
 	}
 	if (msg.unrestricting) msg.unrestricting.forEach(unr => addBar(100, 100, unr));
-	if (msg.downloading) msg.downloading.forEach(unr => addBar(unr.file.bytesWritten, unr.fileSize, (unr.file.path + ': ' + (unr.file.bytesWritten / unr.fileSize * 100) + '% (' + unr.file.bytesWritten + ' of ' + unr.fileSize + ' bytes)')));
+	console.log('debug3:', msg);
+	if (msg.downloading) msg.downloading.forEach(unr => {
+		console.log('debug4:', unr);
+		addBar(unr.file.bytesWritten, unr.fileSize, (unr.file.path + ': ' + (unr.file.bytesWritten / unr.fileSize * 100) + '% (' + unr.file.bytesWritten + ' of ' + unr.fileSize + ' bytes)')));
+	}
 };
