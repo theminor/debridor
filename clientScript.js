6ca9bda9ffb5291df7ce3194e689faf7f6a6697d
John <theminor@users.noreply.github.com>
2018-11-06 13:12:17 -0500

    Clean; add progress bars

--- clientScript.js
+++ clientScript.js
@@ -15,5 +15,19 @@ const skt = new WebSocket(window.location.href.replace('http://', 'ws://').repla
 skt.onmessage = function(event) {
 	let msg = JSON.parse(event.data);
 	let statusElement = document.getElementById('statusText');
+	let progBarsDiv = document.getElementById('progBars');
+	while (progBarsDiv.hasChildNodes()) { progBarsDiv.removeChild(progBarsDiv.lastChild); }  // remove all bars and re-build each, below
+	function addBar(current, max, text) {
+		let newBar = progBarsDiv.appendChild(document.createElement('div'));
+		newBar.className = 'progress-bar';
+		newBar.setAttribute('role', 'progressbar');
+		newBar.setAttribute('width', (current / max * 100).toString() + '%');
+		newBar.setAttribute('aria-valuenow', current / max * 100);
+		newBar.setAttribute('aria-valuemin', 0);
+		newBar.setAttribute('aria-valuemax', max);
+		newBar.innerText = text || ((current / max * 100).toString() + '%');
+	}
+	msg.unrestricting.forEach(unr => addBar(100, 100, unr));
+	msg.downloading.forEach(unr => addBar(unr.file.bytesWritten, unr.fileSize, (unr.file.path + ': ' + (unr.file.bytesWritten / unr.fileSize * 100) + '% (' + unr.file.bytesWritten + ' of ' + unr.fileSize + ' bytes)')));
 	statusElement.innerHTML = statusElement.innerHTML + '\n' + msg;
 };
