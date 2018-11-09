c319bd09c2cec380fad4287ea61093300f8eec56
John <theminor@users.noreply.github.com>
2018-11-09 11:13:35 -0500

    fix error bar text; add surrounding div to each bar

--- clientScript.js
+++ clientScript.js
@@ -21,21 +21,23 @@ skt.onmessage = function(event) {
 		let progBarsDiv = document.getElementById('progBars');
 		while (progBarsDiv.hasChildNodes()) { progBarsDiv.removeChild(progBarsDiv.lastChild); }  // remove all bars and re-build each, below
 		function addBar(current, max, path, style) {
-			let newBar = progBarsDiv.appendChild(document.createElement('div'));
+			let barSurround = progBarsDiv.appendChild(document.createElement('div'));
+			barSurround.className = 'progress';
+			let newBar = barSurround.appendChild(document.createElement('div'));
 			newBar.className = 'progress-bar ' + style;
 			newBar.setAttribute('role', 'progressbar');
 			newBar.setAttribute('style', 'width: ' + (current / max * 100).toString() + '%');
 			newBar.setAttribute('aria-valuenow', current);
 			newBar.setAttribute('aria-valuemin', 0);
 			newBar.setAttribute('aria-valuemax', max);
-			newBar.onclick = () => skt.send(JSON.stringify({ remove: path }));
+			barSurround.onclick = () => skt.send(JSON.stringify({ remove: path }));
 			if (current === 100 && max === 100) newBar.innerText = path;
 			else newBar.innerText = path + ': ' + Math.round(current/max * 100) + '% (' + Math.round(current/1000000) + ' of ' + Math.round(max/1000000) + ' MB)';
 		}
 		if (msg.unrestricting) msg.unrestricting.forEach(unr => addBar(100, 100, unr, 'bg-warning'));
 		if (msg.downloading) msg.downloading.forEach(unr => addBar(unr.file.bytesWritten, unr.fileSize, unr.file.path, 'bg-info'));
 		if (msg.completed) msg.completed.forEach(unr => addBar(100, 100, unr, 'bg-success'));
-		if (msg.errors) msg.errors.forEach(unr => addBar(100, 100, unr, 'bg-danger'));
+		if (msg.errors) msg.errors.forEach(unr => addBar(100, 100, unr.item, 'bg-danger'));
 	} else console.log(msg);
 }
 
