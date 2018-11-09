dcdd344d71015641290461ec675c578e736783f3
John <theminor@users.noreply.github.com>
2018-11-09 11:36:56 -0500

    Add more info to bars

--- clientScript.js
+++ clientScript.js
@@ -34,10 +34,10 @@ skt.onmessage = function(event) {
 			if (current === 100 && max === 100) newBar.innerText = path;
 			else newBar.innerText = path + ': ' + Math.round(current/max * 100) + '% (' + Math.round(current/1000000) + ' of ' + Math.round(max/1000000) + ' MB)';
 		}
-		if (msg.unrestricting) msg.unrestricting.forEach(unr => addBar(100, 100, unr, 'bg-warning'));
+		if (msg.unrestricting) msg.unrestricting.forEach(unr => addBar(100, 100, 'Unrestricting: ' + unr, 'bg-warning'));
 		if (msg.downloading) msg.downloading.forEach(unr => addBar(unr.file.bytesWritten, unr.fileSize, unr.file.path, 'bg-info'));
-		if (msg.completed) msg.completed.forEach(unr => addBar(100, 100, unr, 'bg-success'));
-		if (msg.errors) msg.errors.forEach(unr => addBar(100, 100, unr.item, 'bg-danger'));
+		if (msg.completed) msg.completed.forEach(unr => addBar(100, 100, 'Complete: ' + unr, 'bg-success'));
+		if (msg.errors) msg.errors.forEach(unr => addBar(100, 100, unr.error + ': ' + unr.item, 'bg-danger'));
 	} else console.log(msg);
 }
 
