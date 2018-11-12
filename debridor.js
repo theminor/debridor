d651f76b846456242ec10a102acb06611bb97e41
John <theminor@users.noreply.github.com>
2018-11-12 12:21:39 -0500

    debugging

--- debridor.js
+++ debridor.js
@@ -89,6 +89,9 @@ function logMsg(errOrMsg, reject, linksStatElmnt, ws, level, supressStack) {
 			if (!matchFound && (removeArrayElement(aryName, linksStatElmnt, true))) {
 				matchFound = true;
 				if (aryName !== 'errors') {
+					
+					console.log(errOrMsg);
+					
 					linksStatus.errors.push({ "item": linksStatElmnt, "error": errOrMsg, "date": errDate });  // if it wasn't in the errrors list, add it to the list
 					if (linksStatus.errors.length > settings.server.maxErrLogLength) linksStatus.errors.shift();  // remove top item, if the list is getting too long
 				}
