1b6df32f8d0d5401ab5241495736cbc1cda87954
John <theminor@users.noreply.github.com>
2019-05-07 14:55:37 -0400

    Update clientScript.js

--- clientScript.js
+++ clientScript.js
@@ -1,45 +1,75 @@
-const skt = new WebSocket(window.location.href.replace('http://', 'ws://').replace('https://', 'wss://'));
-
-function submitBtn() {
-	skt.send(JSON.stringify(
-		{
-			links: document.getElementById('linksBox').value.trim().split(/\n/),
-			linksPw: document.getElementById('linkPwInput').value,
-			saveLoc: document.getElementById('saveLocationSelect').value
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>xr18</title>
+	<style>
+		body { font-family: Arial, Helvetica, sans-serif; }
+		h1 { color: maroon; }
+		h2 {
+			color: DarkSlateGray;
+			margin: 40px 0px 0px;
 		}
-	));
-}
-
-function getStatus() {
-	skt.send("{}");
-}
-
-skt.onmessage = function(event) {
-	let msg = JSON.parse(event.data);
-	if (typeof msg === 'string' && msg.startsWith('{')) msg = JSON.parse(msg);  // needed for bug re "over-stringified" json
-	if (typeof msg === 'object') {
-		let progBarsDiv = document.getElementById('progBars');
-		while (progBarsDiv.hasChildNodes()) { progBarsDiv.removeChild(progBarsDiv.lastChild); }  // remove all bars and re-build each, below
-		function addBar(current, max, path, preMsg, style) {
-			let barSurround = progBarsDiv.appendChild(document.createElement('div'));
-			barSurround.className = 'progress mt-3';
-			let newBar = barSurround.appendChild(document.createElement('div'));
-			newBar.className = 'progress-bar ' + style;
-			newBar.setAttribute('role', 'progressbar');
-			newBar.setAttribute('style', 'width: ' + (current / max * 100).toString() + '%');
-			newBar.setAttribute('aria-valuenow', current);
-			newBar.setAttribute('aria-valuemin', 0);
-			newBar.setAttribute('aria-valuemax', max);
-			barSurround.onclick = () => skt.send(JSON.stringify({ remove: path }));
-			if (current === 100 && max === 100) newBar.innerText = preMsg + path;
-			else newBar.innerText = path + ': ' + Math.round(current/max * 100) + '% (' + Math.round(current/1000000) + ' of ' + Math.round(max/1000000) + ' MB)';
+		div {
+			width: 200px;
+			text-align: center;
 		}
-		if (msg.unrestricting) msg.unrestricting.forEach(unr => addBar(100, 100, unr, 'Unrestricting: ', 'bg-warning'));
-		if (msg.downloading) msg.downloading.forEach(unr => addBar(unr.file.bytesWritten, unr.fileSize, unr.file.path, '', 'bg-info'));
-		if (msg.completed) msg.completed.forEach(unr => addBar(100, 100, unr, 'Complete: ', 'bg-success'));
-		if (msg.errors) msg.errors.forEach(unr => addBar(100, 100, unr.item, unr.error + ': ', 'bg-danger'));
-	} else console.log(msg);
-}
-
-setInterval(getStatus, 7500);  // update status every 7.5 seconds
-skt.onopen = getStatus;        // and update once the websocket is open and ready
+		#status {
+			font-family: "Courier New", Courier, monospace;
+			font-size: 20px;
+		}
+		button {
+			background-color: ForestGreen;
+			color: white;
+			font-size: 20px;
+			height: 70px;
+			width: 130px;
+			border-radius: 35%;
+		}
+    .redFont { color: DarkRed; }
+    .greenFont { color: ForestGreen; }
+    table { color: DarkBlue; }
+	</style>
+  <script>
+    const skt = new WebSocket(window.location.href.replace('http://', 'ws://').replace('https://', 'wss://'));
+    let msg = {}; // format: { isRecording: 'recording information', files: ['fileName1', 'fileName2'] }
+    function $(sel) { return document.getElementById(sel); }
+    function getStatus() { skt.send("getStatus"); }
+    skt.onmessage = function(event) {
+      msg = JSON.parse(event.data);
+      if (msg.isRecording) {
+        $('status').innerText = '[Recording]';
+        // *** TO DO display msg.isRecording
+      } else {
+        $('status').innerText = '[Stopped]';
+      }
+      if (msg.files) {
+        const tbl = $('filesTable');
+        tbl.innerHTML = '';
+        for (let i = 0; i < msg.files.length; i++) {
+          tbl.innerHTML += `<tr><td>${msg.files[i]}</td><td class="redFont">[Delete]</td></tr>`;
+        }
+      }      
+    }
+    function startStopBtnClick() {
+      if (msg.isRecording) skt.send("stopRecording");  // *** TO DO - add "are you sure"
+      else skt.send("startRecording");
+    }
+    skt.onopen = getStatus;
+    setInterval(getStatus, 7500);
+  </script>
+</head>
+<body>
+	<h1>xr18</h1>
+	<div>
+		<h3 id="status">[Stopped]</h3>
+		<button id="startStopBtn" onclick="startStopBtnClick()">Record</button>
+	</div>
+	<h2>Files:</h2>
+	<table id="filesTable">
+	  <tr>
+		  <td>File Link</td><td class="redFont">[Delete]</td>
+	  </tr>
+	</table>
+</body>
+</html>
