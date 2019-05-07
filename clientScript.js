5a62583f117241e20b29bbccc54e6b29338fd820
John <theminor@users.noreply.github.com>
2019-05-07 15:43:59 -0400

    Oops - revert to prior - was working in wrong repo

--- clientScript.js
+++ clientScript.js
@@ -1,93 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>xr18</title>
-	<style>
-		body { font-family: Arial, Helvetica, sans-serif; }
-		h1 { color: maroon; }
-		h2 {
-			color: DarkSlateGray;
-			margin: 40px 0px 0px;
+const skt = new WebSocket(window.location.href.replace('http://', 'ws://').replace('https://', 'wss://'));
+
+function submitBtn() {
+	skt.send(JSON.stringify(
+		{
+			links: document.getElementById('linksBox').value.trim().split(/\n/),
+			linksPw: document.getElementById('linkPwInput').value,
+			saveLoc: document.getElementById('saveLocationSelect').value
 		}
-		div {
-			width: 200px;
-			padding-left: 30px;
+	));
+}
+
+function getStatus() {
+	skt.send("{}");
+}
+
+skt.onmessage = function(event) {
+	let msg = JSON.parse(event.data);
+	if (typeof msg === 'string' && msg.startsWith('{')) msg = JSON.parse(msg);  // needed for bug re "over-stringified" json
+	if (typeof msg === 'object') {
+		let progBarsDiv = document.getElementById('progBars');
+		while (progBarsDiv.hasChildNodes()) { progBarsDiv.removeChild(progBarsDiv.lastChild); }  // remove all bars and re-build each, below
+		function addBar(current, max, path, preMsg, style) {
+			let barSurround = progBarsDiv.appendChild(document.createElement('div'));
+			barSurround.className = 'progress mt-3';
+			let newBar = barSurround.appendChild(document.createElement('div'));
+			newBar.className = 'progress-bar ' + style;
+			newBar.setAttribute('role', 'progressbar');
+			newBar.setAttribute('style', 'width: ' + (current / max * 100).toString() + '%');
+			newBar.setAttribute('aria-valuenow', current);
+			newBar.setAttribute('aria-valuemin', 0);
+			newBar.setAttribute('aria-valuemax', max);
+			barSurround.onclick = () => skt.send(JSON.stringify({ remove: path }));
+			if (current === 100 && max === 100) newBar.innerText = preMsg + path;
+			else newBar.innerText = path + ': ' + Math.round(current/max * 100) + '% (' + Math.round(current/1000000) + ' of ' + Math.round(max/1000000) + ' MB)';
 		}
-		#status {
-			font-family: "Courier New", Courier, monospace;
-			font-size: 20px;
-		}
-    #statusTable {
-      font-size: 14px;
-      padding-bottom: 20px;
-    }
-		button {
-			background-color: ForestGreen;
-			color: white;
-			font-size: 20px;
-			height: 70px;
-			width: 130px;
-			border-radius: 35%;
-		}
-    .redFont { color: DarkRed; }
-    .greenFont { color: ForestGreen; }
-    .blueFont { color: DarkBlue; }
-	</style>
-  <script>
-    const skt = new WebSocket(window.location.href.replace('http://', 'ws://').replace('https://', 'wss://'));
-    let msg = {}; // format: { isRecording: 'recording information', files: ['fileName1', 'fileName2'] }
-    function $(sel) { return document.getElementById(sel); }
-    function getStatus() { skt.send("getStatus"); }
-    skt.onmessage = function(event) {
-      const st = $('status');
-      const bt = $('startStopBtn');
-      msg = JSON.parse(event.data);
-      if (msg.isRecording) {
-        st.innerText = '[Recording]';
-        st.className = 'redFont';
-        $('statusTable').innerHTML = `<tr><td>Filename:</td><td>${msg.isRecording.fileName}</td></tr>
-                                      <tr><td>Channels:</td><td>${msg.isRecording.numChannels}</td></tr>
-                                      <tr><td>Time:</td><td>${msg.isRecording.recordTime}</td></tr>
-                                      <tr><td>Size:</td><td>${msg.isRecording.fileSize}</td></tr>
-                                      <tr id="clipCnt"><td>Clips:</td><td>${msg.isRecording.numClips}</td></tr>`;
-        if (msg.isRecording.numClips > 0) $('clipCnt').className = 'redFont';
-        bt.value= 'Stop';
-        bt.style.backgroundColor = 'DarkRed';
-      } else {
-        st.innerText = '[Stopped]';
-        st.className = 'blueFont';
-        bt.value= 'Record';
-        bt.style.backgroundColor = 'ForestGreen';
-      }
-      if (msg.files) {
-        const tbl = $('filesTable');
-        tbl.innerHTML = '';
-        for (let i = 0; i < msg.files.length; i++) {
-          tbl.innerHTML += `<tr><td>${msg.files[i]}</td><td class="redFont">[Delete]</td></tr>`;
-        }
-      }      
-    }
-    function startStopBtnClick() {
-      if (msg.isRecording) skt.send("stopRecording");  // *** TO DO - add "are you sure"
-      else skt.send("startRecording");
-    }
-    skt.onopen = getStatus;
-    setInterval(getStatus, 7500);
-  </script>
-</head>
-<body class="blueFont">
-	<h1>xr18</h1>
-	<div>
-		<h3 id="status">[Stopped]</h3>
-    <table id="statusTable"></table>
-		<button id="startStopBtn" onclick="startStopBtnClick()">Record</button>
-	</div>
-	<h2>Files:</h2>
-	<table id="filesTable">
-	  <tr>
-		  <td>File Link</td><td class="redFont">[Delete]</td>
-	  </tr>
-	</table>
-</body>
-</html>
+		if (msg.unrestricting) msg.unrestricting.forEach(unr => addBar(100, 100, unr, 'Unrestricting: ', 'bg-warning'));
+		if (msg.downloading) msg.downloading.forEach(unr => addBar(unr.file.bytesWritten, unr.fileSize, unr.file.path, '', 'bg-info'));
+		if (msg.completed) msg.completed.forEach(unr => addBar(100, 100, unr, 'Complete: ', 'bg-success'));
+		if (msg.errors) msg.errors.forEach(unr => addBar(100, 100, unr.item, unr.error + ': ', 'bg-danger'));
+	} else console.log(msg);
+}
+
+setInterval(getStatus, 7500);  // update status every 7.5 seconds
+skt.onopen = getStatus;        // and update once the websocket is open and ready
